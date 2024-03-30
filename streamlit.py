588977982283deb2e9c521b3ccd50f03b9fc4966
AtulkrishnanMU <115357632+AtulkrishnanMU@users.noreply.github.com>
2024-03-31 00:12:23 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -20,9 +20,14 @@ current_datetime = datetime.datetime.now()
 # Format it as a string including seconds and with the desired format
 current_date = current_datetime.strftime("%dth %b %H:%M:%S")
 
-# Create or connect to the SQLite database
-conn = sqlite3.connect('users.db')
-c = conn.cursor()
+@st.cache(allow_output_mutation=True)  # Cache the connection object
+def get_database_connection():
+    conn = sqlite3.connect('users.db')
+    c = conn.cursor()
+    return conn, c
+
+# Get the cached connection object
+conn, c = get_database_connection()
 
 # Create a table to store user data if it doesn't exist
 c.execute('''
