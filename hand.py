87a84bd70bf36cfbcc6942db95cf7ce370139ecc
AtulkrishnanMU <115357632+AtulkrishnanMU@users.noreply.github.com>
2024-04-30 23:58:20 +0530

    Update hand.py

--- hand.py
+++ hand.py
@@ -303,7 +303,7 @@ def hand(vid, loading_bar_hand):
             break
 
         debug_image = copy.deepcopy(image)
-        debug_image = cv2.cvtColor(debug_image, cv2.COLOR_BGR2RGB)
+        debug_image = cv.cvtColor(debug_image, cv.COLOR_BGR2RGB)
 
         height, width = debug_image.shape[:2]
 
